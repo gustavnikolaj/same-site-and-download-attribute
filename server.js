aba335d5b90a67ff3eb6222207829b04c814d564
Gustav Nikolaj Olsen <gno@one.com>
2016-10-26 11:29:20 +0200

    add download links with and without download attribute

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ app
     console.log('%s: %s', req.method, req.url)
     next();
   })
+  .use('/download.txt', function (req, res, next) {
+    if (req.query.forceDownload) {
+      res.set('Content-Disposition', 'attachment');
+    }
+    next();
+  })
   .get('/login', function (req, res, next) {
     res.send(`
       <div>
       <form action="/loginLax" method="POST">
         <button type="submit">Login Lax</button>
+      </form>
+
+      <form action="/loginStrict" method="POST">
+        <button type="submit">Login Strict</button>
       </form>
 
       <form action="/login" method="POST">
@@ -26,9 +36,16 @@ app
     res.redirect(302, '/');
   })
   .post('/loginLax', function (req, res, next) {
-    res.cookie('testCookie', 'lax', {
+    res.cookie('testCookie', 'Lax', {
       path: '/',
       sameSite: 'Lax'
+    });
+    res.redirect(302, '/');
+  })
+  .post('/loginStrict', function (req, res, next) {
+    res.cookie('testCookie', 'Strict', {
+      path: '/',
+      sameSite: 'Strict'
     });
     res.redirect(302, '/');
   })
@@ -39,16 +56,13 @@ app
     return next();
   })
   .get('/logout', function (req, res, next) {
-    var cookieOpts = req.cookies.testCookie === 'lax' ? { sameSite: 'lax' } : {};
-    res.cookie('testCookie', '', Object.assign(cookieOpts, {
-      path: '/',
-      expires: new Date(1)
-    }));
+    res.clearCookie('testCookie');
     res.redirect(302, '/login');
   })
   .get('/', function (req, res, next) {
     res.send(`
-      <a href="/download.txt" download>Download</a><br>
+      <a href="/download.txt" download>A tag with download attribute</a><br>
+      <a href="/download.txt?forceDownload=true">A tag without download attribute</a><br>
       <a href="/logout">Logout</a>
     `);
   })
